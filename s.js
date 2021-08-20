a474d12e4aed8682af65f294287043fd042f116e
sdeseille <sebastien.deseille@gmail.com>
2021-08-20 22:29:23 +0200

    Update s.js

--- s.js
+++ s.js
@@ -6,8 +6,8 @@ let sprite = Sprite({
   x: 100,        // starting x,y position of the sprite
   y: 80,
   color: 'red',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
+  width: 10,     // width and height of the sprite rectangle
+  height: 15,
   dx: 2          // move the sprite 2px to the right every frame
 });
 
