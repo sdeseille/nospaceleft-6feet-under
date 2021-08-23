b4531923c305b76ccf3fd93d8ae343ca9ddcda24
sdeseille <sebastien.deseille@gmail.com>
2021-08-23 23:40:39 +0200

    added some style to the canvas

--- src/app/main.js
+++ src/app/main.js
@@ -4,16 +4,24 @@ let { init, Sprite, GameLoop, initKeys, keyPressed } = kontra
 let { canvas } = init();
 
 let sprite = Sprite({
-x: 100,        // starting x,y position of the sprite
-y: 80,
-color: 'red',  // fill color of the sprite rectangle
-width: 10,     // width and height of the sprite rectangle
-height: 15,
-dx: 2          // move the sprite 2px to the right every frame
+  x: 100,        // starting x,y position of the sprite
+  y: 80,
+  color: 'red',  // fill color of the sprite rectangle
+  width: 10,     // width and height of the sprite rectangle
+  height: 15,
+  dx: 2          // move the sprite 2px to the right every frame
+});
+
+let ground = Sprite({
+  type: 'ground',
+  render() {
+      this.context.fillStyle = 'forestgreen';
+      this.context.fillRect(0, canvas.height, canvas.width, -50);
+  }
 });
 
 let loop = GameLoop({  // create the main game loop
-update: function() { // update the game state
+  update: function() { // update the game state
     sprite.update();
 
     // wrap the sprites position when it reaches
@@ -21,10 +29,11 @@ update: function() { // update the game state
     if (sprite.x > canvas.width) {
     sprite.x = -sprite.width;
     }
-},
-render: function() { // render the game state
+  },
+  render: function() { // render the game state
+    ground.render();
     sprite.render();
-}
+  }
 });
 
 loop.start();    // start the game
