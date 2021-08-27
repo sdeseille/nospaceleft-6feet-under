5ed1f522c665967cf118373b6ec83fb8342a71db
sdeseille <sebastien.deseille@gmail.com>
2021-08-28 00:07:24 +0200

    i can see the 3 floors

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,5 @@
 import kontra from './libs/kontra.min.mjs'
-let { init, Sprite, GameLoop, initKeys, keyPressed } = kontra
+let { init, Sprite, GameLoop, initKeys, keyPressed, TileEngine } = kontra
 
 let { canvas } = init();
 
@@ -12,16 +12,71 @@ let sprite = Sprite({
   dx: 2          // move the sprite 2px to the right every frame
 });
 
-let ground = Sprite({
+let platforms = []
+let deepness = 5
+let tunnelPos = 0
+
+let floor_one = Sprite({
   type: 'ground',
+  name: 'platform-1',
+  x: 0,
+  y: 20,
   render() {
-      this.context.fillStyle = 'forestgreen';
-      this.context.fillRect(0, canvas.height, canvas.width, -50);
+    this.context.fillStyle = 'forestgreen';
+    this.context.fillRect(this.x, this.y , canvas.width, -10);
   }
 });
 
+let floor_two = Sprite({
+  type: 'ground',
+  name: 'platform-2',
+  x: 0,
+  y: 60,
+  render() {
+    this.context.fillStyle = 'gray';
+    this.context.fillRect(this.x, this.y , canvas.width, -10);
+  }
+});
+
+let floor_three = Sprite({
+  type: 'ground',
+  name: 'platform-3',
+  x: 0,
+  y: 100,
+  render() {
+    this.context.fillStyle = 'olive';
+    this.context.fillRect(this.x, this.y , canvas.width, -10);
+  }
+});
+
+for (let n = 0; n < deepness; n++){
+  let ground = Sprite({
+    type: 'ground',
+    name: 'platform-'+n,
+    x: 0,
+    y: tunnelPos,
+    render() {
+      this.context.fillStyle = 'forestgreen';
+      this.context.fillRect(0, tunnelPos , canvas.width, -10);
+    }
+  });
+  console.log(ground.name)
+  platforms.push(ground)
+  tunnelPos = tunnelPos + (canvas.height / deepness)
+}
+
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
+
+    // for (let t = 0; t < platforms.length; t++){
+    //   console.log(platforms[t].y)
+    //   platforms[t].update()
+    // }
+
+    floor_one.update();
+    floor_two.update();
+    floor_three.update();
+
     sprite.update();
 
     // wrap the sprites position when it reaches
@@ -31,7 +86,13 @@ let loop = GameLoop({  // create the main game loop
     }
   },
   render: function() { // render the game state
-    ground.render();
+    // for (let t = 0; t < platforms.length; t++){
+    //   console.log(platforms[t].y)
+    //   platforms[t].render()
+    // }
+    floor_one.render();
+    floor_two.render();
+    floor_three.render();
     sprite.render();
   }
 });
